0299bb61aa0b66df6b21256d22328e7664db9443
Felix georgeault <georgeault.felix@gmail.com>
2022-04-15 19:54:58 +0200

    homepage style

--- views/layouts/default.php
+++ views/layouts/default.php
@@ -5,22 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Solidarité | <?= $pageTitle ?? 'Home' ?></title>
-    <link rel="stylesheet" href="cs/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/style.css">
 </head>
-<body class="d-flex flex-column h-100">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a href="#" class="navbar-brand ms-4">Mon site</a>
+<body>
+    <nav class="navbar flex">
+        <div class="nav-links">
+            <ul>
+                <li><h4><a class="underline" href="#">Accueil</a></h4></li>
+                <li><h4><a href="/blog#event">Blog</a></h4></li>
+                <li><h4><a href="#">A propos</a></h4></li>
+                <li><h4><a href="#">Contact</a></h4></li>
+            </ul>
+        </div>
+        <div><a href="#">test</a></div>
     </nav>
-    <div class="container mt-4">
-        <?= $content ?>
-    </div>
-    <footer class="bg-light py-4 footer mt-auto">
-        <div class="container">
+    <?= $content ?>
+    <footer>
+        <!-- <div class="container">
             <?php if(defined('DEBUG_TIME')) : ?>
                 Page générée en <?= round(1000 * (microtime(true) - DEBUG_TIME)) ?> ms
-            <?php endif ?>
-        </div>
+            <?php endif ?> 
+        </div> -->
     </footer>
 </body>
 </html>
--- views/post/index.php
+++ views/post/index.php
@@ -2,4 +2,155 @@
 $pageTitle = "Mon blog";
 ?>
 
-<h1>Mon Blog</h1>
+<div class="banner"></div>
+
+<section class="container home-little-grid">
+    <div class="card home-card-big relative">
+        <h1>Bienvenue sur le site de la solidarité 
+            de l’athénée Royal d’Uccle 1    
+        </h1>
+        <h2>
+            Que vais-je trouver sur ce <strong>blog</strong> ?  
+        </h2>
+        <h4>
+            Ici vous retrouverez tous les évenements se passant <br>
+            dans l’athénée concernant la solidarité, comme par exemple <br>
+            la semaine de la solidarité ou encore les shoeboxs.
+        </h4>
+    </div>
+    <div class="card home-card-little">
+        <h2>
+            Comment ça <strong>fonctionne</strong> ? 
+        </h2>
+        <h4 class="silent">
+            Ce strong est en quelque sorte un journal des événements. 
+            De nombreux posts vont être ajoutés au fil du temps pour en garder une trace.
+             Dans ces posts il y aura du texte, photos, vidéos voire même documents.
+        </h4>
+    </div>
+</section>
+
+<section class="event">
+    <div class="header-section flex">
+        <h2 id="event">Voici les <strong>derniers</strong> évènements</h2>
+        <p class="mobile-hidden">Mis à jour le 02/03/2022</p>
+    </div>
+    <div class="big-grid-event">
+        <div class="card event-card">
+                <h4>
+                    Exemple Titre
+                </h4>
+                <p>
+                    Lörem ipsum nira pneumalog. 
+                    Astrolara antivaxxare klustersmitta, asm vid. 
+                    Tens trerade kilagt mint. Infragt deras IVPA tragisk tikaning.
+                    Besm krospenera dogon i vilektiga. 
+                    Diktig Maudeffekten tär som paras. 
+                    Nifeporade mafapp i rena i lyrtad: teng. 
+                </p>
+                <a href="#"><button class="btn btn-swap f-right">Bouton</button></a>
+        </div>   
+        <div class="card event-card">
+                <h4>
+                    Exemple Titre
+                </h4>
+                <p>
+                    Lörem ipsum nira pneumalog. 
+                    Astrolara antivaxxare klustersmitta, asm vid. 
+                    Tens trerade kilagt mint. Infragt deras IVPA tragisk tikaning.
+                    Besm krospenera dogon i vilektiga. 
+                    Diktig Maudeffekten tär som paras. 
+                    Nifeporade mafapp i rena i lyrtad: teng. 
+                </p>
+                <a href="#"><button class="btn btn-swap f-right">Bouton</button></a>
+        </div>  
+        <div class="card event-card">
+                <h4>
+                    Exemple Titre
+                </h4>
+                <p>
+                    Lörem ipsum nira pneumalog. 
+                    Astrolara antivaxxare klustersmitta, asm vid. 
+                    Tens trerade kilagt mint. Infragt deras IVPA tragisk tikaning.
+                    Besm krospenera dogon i vilektiga. 
+                    Diktig Maudeffekten tär som paras. 
+                    Nifeporade mafapp i rena i lyrtad: teng. 
+                </p>
+                <a href="#"><button class="btn btn-swap f-right">Bouton</button></a>
+        </div>  
+        <div class="card event-card">
+                <h4>
+                    Exemple Titre
+                </h4>
+                <p>
+                    Lörem ipsum nira pneumalog. 
+                    Astrolara antivaxxare klustersmitta, asm vid. 
+                    Tens trerade kilagt mint. Infragt deras IVPA tragisk tikaning.
+                    Besm krospenera dogon i vilektiga. 
+                    Diktig Maudeffekten tär som paras. 
+                    Nifeporade mafapp i rena i lyrtad: teng. 
+                </p>
+                <a href="#"><button class="btn btn-swap f-right">Bouton</button></a>
+        </div>  
+        <div class="card event-card">
+                <h4>
+                    Exemple Titre
+                </h4>
+                <p>
+                    Lörem ipsum nira pneumalog. 
+                    Astrolara antivaxxare klustersmitta, asm vid. 
+                    Tens trerade kilagt mint. Infragt deras IVPA tragisk tikaning.
+                    Besm krospenera dogon i vilektiga. 
+                    Diktig Maudeffekten tär som paras. 
+                    Nifeporade mafapp i rena i lyrtad: teng. 
+                </p>
+                <a href="#"><button class="btn btn-swap f-right">Bouton</button></a>
+        </div>  
+        <div class="card event-card">
+                <h4>
+                    Exemple Titre
+                </h4>
+                <p>
+                    Lörem ipsum nira pneumalog. 
+                    Astrolara antivaxxare klustersmitta, asm vid. 
+                    Tens trerade kilagt mint. Infragt deras IVPA tragisk tikaning.
+                    Besm krospenera dogon i vilektiga. 
+                    Diktig Maudeffekten tär som paras. 
+                    Nifeporade mafapp i rena i lyrtad: teng. 
+                </p>
+                <a href="#"><button class="btn btn-swap f-right">Bouton</button></a>
+        </div>  
+        <div class="card event-card">
+                <h4>
+                    Exemple Titre
+                </h4>
+                <p>
+                    Lörem ipsum nira pneumalog. 
+                    Astrolara antivaxxare klustersmitta, asm vid. 
+                    Tens trerade kilagt mint. Infragt deras IVPA tragisk tikaning.
+                    Besm krospenera dogon i vilektiga. 
+                    Diktig Maudeffekten tär som paras. 
+                    Nifeporade mafapp i rena i lyrtad: teng. 
+                </p>
+                <a href="#"><button class="btn btn-swap f-right">Bouton</button></a>
+        </div>  
+        <div class="card event-card">
+                <h4>
+                    Exemple Titre
+                </h4>
+                <p>
+                    Lörem ipsum nira pneumalog. 
+                    Astrolara antivaxxare klustersmitta, asm vid. 
+                    Tens trerade kilagt mint. Infragt deras IVPA tragisk tikaning.
+                    Besm krospenera dogon i vilektiga. 
+                    Diktig Maudeffekten tär som paras. 
+                    Nifeporade mafapp i rena i lyrtad: teng. 
+                </p>
+                <a href="#"><button class="btn btn-swap f-right">Bouton</button></a>
+        </div>  
+    </div>
+</section>
+
+
+
+
