a9cca18b542f95d5cee8aa8951a6129a83e0b5df
Felix georgeault <georgeault.felix@gmail.com>
2022-05-02 02:17:20 +0200

    style hotfix + refactor + authors in composer.json

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Site de la Solidarité
 
-** Version 1.1.5
+** Version 1.1.6
 
 ## Description
 
--- views/admin/post/index.php
+++ views/admin/post/index.php
@@ -16,14 +16,14 @@ $link = $router->url('admin_posts');
 <?php endif ?>
     <section class="post-listing">
         <div class="post-listing__header">
-            <h3>#</h3>
-            <h3>Titre</h3>
+            <h3 class="mobile-hidden">#</h3>
+            <h3 class="mobile-hidden">Titre</h3>
             <a href="<?= $router->url('admin_post_new') ?>" class="btn btn-primary new-article">Ajouter un article</a>
         </div>
         <section class="post-listing__body">
             <?php foreach ($posts as $post): ?>
                 <div class="card-design admin-card">
-                    <h4 class="admin-card__id"><?= e($post->getID()) ?></h4>
+                    <h4 class="admin-card__id mobile-hidden"><?= e($post->getID()) ?></h4>
                     <h4 class="admin-card__title"><a
                                 href="<?= $router->url('admin_post', ['id' => $post->getID()]) ?>"><?= e($post->getName()) ?></a>
                     </h4>
