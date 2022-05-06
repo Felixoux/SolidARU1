ab452dc6609bbcd6f80f96e40b481169bdee9bf6
Felix georgeault <georgeault.felix@gmail.com>
2022-05-07 00:32:51 +0200

    add admin_guide page for parsedown tuto

--- views/admin/post/_form.php
+++ views/admin/post/_form.php
@@ -4,6 +4,7 @@
         <?= $form->input('slug', 'URL') ?>
 <!--         --><?//= $form->file('image', 'Image à la une') ?>
         <?= $form->select('categories_ids', 'Catégories', $categories) ?>
+        <a class="underline" href="<?= $router->url('admin_guide') ?>">Guide d'écriture simplifié (pour le contenu uniquement)</a>
         <?= $form->textarea('content', 'Contenu') ?>
         <?= $form->input('created_at', 'Date de publication') ?>
         <button class="btn-primary">
