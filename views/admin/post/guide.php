a7ed5427d6afc14a4403eda5d828c73577b540e8
Felix georgeault <georgeault.felix@gmail.com>
2022-05-22 01:29:18 +0200

    begin full guide

--- views/admin/post/guide.php
+++ views/admin/post/guide.php
@@ -6,7 +6,73 @@ Auth::check();
 ?>
 
 <section class="guide">
-    <h1 class="section-title">Guide d'écriture <strong>simplifiée</strong></h1>
+    <h1 class="big-title"><strong>Guide</strong> d'administration du site</h1>
+    <h2>Sommaire</h2>
+    <ul class="summary-titles">
+        <li><h3 class="medium-title"><a href="#login">1. Connection/déconnection</a></h3></li>
+        <li><h3 class="medium-title"><a href="#create-article">2. Créer un article</a></h3></li>
+        <li><h3 class="medium-title"><a href="#">3. Créer une catégorie</a></h3></li>
+        <li><h3 class="medium-title"><a href="#">4. Ajouter des fichiers</a></h3></li>
+        <li><h3 class="medium-title"><a href="#">5. Suppression des éléments</a></h3></li>
+        <li><h3 class="medium-title"><a href="#">6. Gérer le mot de passe</a></h3></li>
+    </ul>
+
+    <div id="login" class="guide-group">
+
+    <h2>Connection/déconnection</h2>
+    <hr>
+    <h4>Pour se connecter il faut: </h4>
+    <ul>
+        <li>Taper “/admin” à la fin du l’url de l’accueil, soit “felixoux.alwaysdata.net”.
+            Ce qui donne “felixoux.alwaysdata.net/admin”.</li>
+        <li>Ensuite entrez le nom d’utilisateur, par défaut “admin”.</li>
+        <li>Et entrez le mot de passe, par défaut “admin”.</li>
+        <li>Vous pouvez éventuellement cliquer sur “RESET CONNECTER”.
+            Cela va retenir votre état de connection pendant plusieurs jours, plus pratique
+            que se connecter tout le temps.
+        </li>
+    </ul>
+    <h4>Une fois connecté :</h4>
+    <ul>
+        <li>Vous avez maintenant accès au panel administration ainsi que sa naviguation. </li>
+        <li>Pour revenir sur le site normal (celui que l’utilisateur standart vois), cliquez sur la maison
+            dans la navigution. </li>
+        <li>Vous aurez aussi un nouvel onglet “admin” qui apparaitera dans la naviguation
+            du site standart une fois connecté.
+            Il est là pour éviter de devoir rentrer “/admin” à chaque fois qu’on veut retourner dans
+            le panel admin.</li>
+        <li>Et bien sur vous pouvez vous déconnecter à tout moment en cliquant sur le logo de
+            déconnection
+            à droite de la navigation.
+        </li>
+    </ul>
+    </div>
+    <div id="create-article" class="guide-group">
+        <h2>Créer un article</h2>
+        <hr>
+        <h4>Pour créer un article, il vous faut faire :</h4>
+        <ul>
+            <li>Aller dans l’onglet “article” de la naviguation et cliquer sur le bouton “ajouter un article”</li>
+            <li>Entrer d’abord le titre de l’article tout simplement.</li>
+            <li>Ensuite vous pouvez rentrez une URL personnalisée. Par defaut elle se complète toute seule avec le nom de l’article.</li>
+            <li>Ensuite vous pouvez écrire du contenu, si vous désirez stylisé le contenu, voici comment faire.</li>
+            <li>Vous devez maintenant associer l’article à une ou plusieurs catégorie.</li>
+        </ul>
+        <h4>Maintenant pour les champs optionnels :</h4>
+        <ul>
+            <li>Vous pouvez ajouter une image à la une, cette image apparaitra sur la carte de l’article.</li>
+            <li>Vous pouvez associés de multiples images au contenu de l’article en les séléctionnant. </li>
+            <li>L’association des documents fonctionne comme les images. </li>
+            <li>Vous n’avez pas d’images à associer ? C’est normal, il faut mettre en ligne. Voir ici comment mettre en lignes
+                les images et les documents.</li>
+            <li>Vous pouvez choisir la date de publication de l’article en dernier. Par defaut la date sera celle du jour actuel.</li>
+        </ul>
+        <h4>Et maintenant, cliquez sur le bouton “ajouter” !</h4>
+    </div>
+
+
+
+    <h2>Guide d'écriture <strong>simplifiée</strong></h2>
     <p class="alert">Attention, l'espace entre les caractères spéciaux et le texte est très important !<br>
         Exemple : #Titre est différent de # Titre
     </p>
