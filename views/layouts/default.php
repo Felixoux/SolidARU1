797fced34b4a639a9e8b30f03ec31dec129261b8
Felix georgeault <georgeault.felix@gmail.com>
2022-05-22 03:22:26 +0200

    Revert "Revert "light/dark theme done""
    This reverts commit 7bb9e7d01c194cba7b7152c265d48458899c588e.

--- views/layouts/default.php
+++ views/layouts/default.php
@@ -105,7 +105,6 @@ if (isset($_COOKIE['theme']) && isset($_COOKIE['theme']) == 'light') {
     $content
     ?>
     <footer class="page-footer">
-
         <svg id="goTopButton" width="48px" height="48px" viewBox="0 0 48 48" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path d="M39.6339 31.8839C39.1457 32.372 38.3543 32.372 37.8661 31.8839L24 18.0178L10.1339 31.8839C9.64573 32.372 8.85427 32.372 8.36612 31.8839C7.87796 31.3957 7.87796 30.6043 8.36612 30.1161L23.1161 15.3661C23.6043 14.878 24.3957 14.878 24.8839 15.3661L39.6339 30.1161C40.122 30.6043 40.122 31.3957 39.6339 31.8839Z"/>
         </svg>
@@ -117,9 +116,8 @@ if (isset($_COOKIE['theme']) && isset($_COOKIE['theme']) == 'light') {
         <div class="RGPD-container flex-center">
             <div class="theme-switcher-container">
                 <div class="theme-switcher form-switch container">
-                    <input type="checkbox" id="theme-switcher" name="theme-switcher" value="1" aria-label="Changer le thème"
-                           checked>
                     <label for="theme-switcher">
+                        <input type="checkbox" id="theme-switcher" name="theme-switcher" value="1" aria-label="Changer le thème" checked>
                         <span class="switch"></span>
                     </label>
                 </div>
