b3a7e4e663ab597e5dd306843bfcfb0bf4d2f5e1
Felix georgeault <georgeault.felix@gmail.com>
2022-05-25 19:00:27 +0200

    hotfixes + better slick slider + better page header

--- views/about.php
+++ views/about.php
@@ -1,5 +1,5 @@
 <div class="container">
-    <button class="btn-primary hidden-btn-js mb3" style="opacity:1; cursor: pointer">c</button>
+    <button class="btn-primary hidden-btn-js mb3" style="opacity:0; cursor: pointer">c</button>
     <img class="hidden hide-content" src="/img/droe.JPG" alt="droedroe-meme" width="350">
 </div>
 
--- views/post/show.php
+++ views/post/show.php
@@ -26,24 +26,12 @@ if ($post->getSlug() !== $slug) {
     exit();
 }
 ?>
-<header class="article__header pager-header flex">
+<header class="article__header page-header flex">
     <h1 class="article__title section-title">
         <?= Text::strong(3, $post->getName()) ?>
     </h1>
     <p class="mobile-hidden muted"><?= $post->getCreatedAt()->format("d/m/Y") ?></p>
 </header>
-    <div class="carroussel-container">
-        <?php foreach ($images as $k => $image) {
-            $name = $image['name'];
-            $link = $router->url('image') . "?name=".$name."&width=10&height=10";
-            echo <<<HTML
-        <div class="blur-img">
-            <img class="lazy" src="$link" alt="$name" data-name="$name" width="350" loading="eager">
-        </div>
-    HTML;
-        }
-        ?>
-    </div>
 <section class="article">
     <?php if ($post->getImage()): ?>
         <!--<img src="<?/*= $post->getImageURL('small') */?>" alt="">-->
@@ -52,6 +40,8 @@ if ($post->getSlug() !== $slug) {
         <?= $post->getBody() ?>
     </div>
     <div class="article__files">
+        <hr>
+        <h3 class="medium-title mb3">Document(s) disponible(s) :</h3>
         <?php foreach ($files as $k => $file) {
             $name = $file['name'];
             $link = '/uploads/files' . DIRECTORY_SEPARATOR . $file['name'];
@@ -63,10 +53,20 @@ if ($post->getSlug() !== $slug) {
         }
         ?>
     </div>
-    <a href="#">
-        <button class="article__button f-right"><a href="<?= $_SESSION['category_link'] ?>">Revenir à la catégorie</a></button>
-    </a>
+    <a class="article__button btn-primary-outline" href="<?= $_SESSION['category_link'] ?>">Revenir à la catégorie</a>
 </section>
+<div class="carroussel-container my5" style="background: var(--bg-card);border: 1px solid var(--border)">
+    <?php foreach ($images as $k => $image) {
+        $name = $image['name'];
+        $link = $router->url('image') . "?name=".$name."&width=10&height=10";
+        echo <<<HTML
+    <div class="blur-img">
+        <img class="lazy" src="$link" alt="$name" data-name="$name" width="350" loading="eager">
+    </div>
+HTML;
+    }
+    ?>
+</div>
 <?php
 // Slick carousel
 $css_slick = <<<HTML
@@ -81,9 +81,11 @@ $js_slick = <<<JS
     $('.carroussel-container').slick({
     infinite: true,
     speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true
+    slidesToShow: 1
         })
+             
+    $('.slick-prev').html('&lt;')
+    $('.slick-next').html('&gt;')
     })
 </script>
 JS;
