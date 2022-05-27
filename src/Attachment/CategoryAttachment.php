84ebc8bc8517eb62c580c4f2f557f233a8a01358
Felix georgeault <georgeault.felix@gmail.com>
2022-05-27 02:45:38 +0200

    hotfix

--- src/Attachment/CategoryAttachment.php
+++ src/Attachment/CategoryAttachment.php
@@ -5,4 +5,5 @@ namespace App\Attachment;
 class CategoryAttachment extends Attachment
 {
     protected string $path = UPLOAD_PATH . DIRECTORY_SEPARATOR . "categories";
+    protected array $formats = ['small'];
 }
