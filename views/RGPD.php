732f01c5fdd26e5f08806e198834e967225caf01
Felix georgeault <georgeault.felix@gmail.com>
2022-06-23 16:06:09 +0200

    add rgpd view

--- views/RGPD.php
+++ views/RGPD.php
@@ -1,11 +1,40 @@
 <?php
-
+$pageTitle = "Mentions légales et politique de confidentialité";
 ?>
 
 <header class="page-header">
-    <h1 class="big-title">Politique de <strong>confidentialité</strong></h1>
+    <h1 class="big-title">Mentions légales et politique de <strong>confidentialité</strong></h1>
 </header>
-
-<section class="rgpd big-section">
-
+<section class="rgpd big-section stack">
+    <div class="rgpd-article">
+        <h3 class="rgpd-article__title">Protection des données à caractère personnel</h3>
+        <div class="rgpd-article__content">
+            <p>Aucune donnée reçue de votre part, n'est enregistrée dans les fichiers d'administration du site. Notre politique de confidentialité respecte le nouveau règlement européen sur la protection des données (RGPD) qui a été définitivement adopté par le Parlement européen le 14 avril 2016.</p>
+        </div>
+    </div>
+    <div class="rgpd-article">
+        <h3 class="rgpd-article__title">Cookies</h3>
+        <div class="rgpd-article__content">
+            <p>Le site ne contient aucun cookie sauvegardant vos données personnelles.</p>
+            <p>Si vous changez le theme de couleur sur le site, un cookie temporaire sera créé afin de retenir votre préférence à travers les pages. Le cookie sera automatiquement détruit après 3 jours.</p>
+        </div>
+    </div>
+    <div class="rgpd-article">
+        <h3 class="rgpd-article__title">Droit des personnes sur leur image</h3>
+        <div class="rgpd-article__content">
+            <p>Ce site contient une partie blog qui peut contenir des photos. Des élèves figurent inévitablement sur ces photos. Nous nous permettons de publier celles-ci dans un but d'information, considérant que ces prises de vue ne relèvent pas de la sphère privée et n'ont pas été prises à l'insu des personnes. Cependant, si figurant sur l'une de celles-ci, vous souhaitez sa suppression, nous le ferons bien volontiers.</p>
+        </div>
+    </div>
+    <div class="rgpd-article">
+        <h3 class="rgpd-article__title">Éditeur responsable, directeur de publication, responsable de la rédaction</h3>
+        <div class="rgpd-article__content">
+            <p>L'éditeur responsable, le directeur de publication et le responsable de la rédaction du site est Madame Vanhessen, Directrice de l'association "Les Amis de l’Athénée Royal d’Uccle 1" - ADRESSE À DEMANDER PENDANT L'APPEL.</p>
+        </div>
+    </div>
+    <div class="rgpd-article">
+        <h3 class="rgpd-article__title">Réclamation</h3>
+        <div class="rgpd-article__content">
+            <p>Pour toute réclamation concernant le droit des personnes sur leur image, veuillez utiliser notre page "contact".</p>
+        </div>
+    </div>
 </section>
