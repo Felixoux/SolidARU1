44269c7af343cdb0b41fff9c08282991e585f8ad
Felix georgeault <georgeault.felix@gmail.com>
2022-06-25 20:59:43 +0200

    add about view

--- views/about.php
+++ views/about.php
@@ -2,8 +2,20 @@
 $pageTitle = "A propos";
 ?>
 <header class="page-header">
-    <h1 class="big-title">Page en construction...</h1>
+    <h1 class="big-title"><strong>Les Amis</strong> de l’Athénée Royal d’Uccle 1, c'est quoi ?</h1>
 </header>
+<section class="big-section stack about-section">
+    <h3 class="medium-title">L’Association « Les Amis de l’Athénée Royal d’Uccle 1 » a pour but de favoriser le développement et le rayonnement de l'enseignement donné à l'Athénée Royal d’Uccle 1</h3>
+    <hr class="w100">
+    <h4>Les objectifs poursuivis par les Amis sont multiples :</h4>
+    <ul>
+        <li>Les Amis apportent une <span class="italic">aide sociale aux familles en difficultés financières</span> (contribution aux frais scolaires, aux repas chauds mais aussi un soutien financier pour la participation aux voyages scolaires)</li>
+        <li>L’association <span class="italic">achète du matériel</span> dont les livres ainsi que du matériel sportif.</li>
+        <li>Elle <span class="italic">organise le prêt du livre</span> chaque année et offre également les prix lors des proclamations.</li>
+        <li>Elle soutient les événements fédérateurs au sein de l’école tels que la fête et le théâtre en intervenant par exemple, dans les frais de décors, costumes et le matériel technique.</li>
+    </ul>
+    <p>Pour pouvoir atteindre ces objectifs, Les Amis organisent notamment une <strong>tombola</strong> et un <strong>bar</strong> lors des activités festives.</p>
+</section>
 
 
 
